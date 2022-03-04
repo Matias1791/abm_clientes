2de3b7e6ef151eb887f73698e0097a58bf13d05a
Matias1791 <moscatachado@hotmail.com>
2022-03-04 00:51:15 -0300

    cambios

--- index.php
+++ index.php
@@ -13,29 +13,32 @@ if(file_exists("archivo.txt")){
 $id = isset($_GET["id"])? $_GET["id"] : "";
 
 if(isset($_GET["do"]) && $_GET["do"] == "eliminar"){
-  
+
     if(file_exists("imagenes/" . $aClientes[$id]["imagen"])){
         unlink("imagenes/" . $aClientes[$id]["imagen"]);     
     }
-    
+
+
     unset($aClientes[$id]);
     $strJson = json_encode($aClientes);
     file_put_contents("archivo.txt", $strJson);
-    header("Location: index.php");
-
+    header("refresh:2; url=index.php");
 }
-
 
 if($_POST){
     $dni = trim($_POST["txtDni"]);
     $nombre = trim($_POST["txtNombre"]);
     $telefono = trim($_POST["txtTelefono"]);
     $correo = trim($_POST["txtCorreo"]);
+    $imagen = "";
 
     if($_FILES["archivo"]["error"] === UPLOAD_ERR_OK){
         if(isset($aClientes[$id]["imagen"]) && $aClientes[$id]["imagen"] != ""){
             if(file_exists("imagenes/" . $aClientes[$id]["imagen"])){
                 unlink("imagenes/" . $aClientes[$id]["imagen"]);
+            }
+            if ($_FILES["archivo"]["error"] != UPLOAD_ERR_OK) {
+                $imagen = "";
             }
         }
         $nombreAleatorio = date("Ymdhmsi");
@@ -94,6 +97,15 @@ if($_POST){
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                 </svg>
                     <div class="ps-2">¡Guardado con éxito!</div>
+            </div>
+        <?php } ?>
+        <?php if(isset($_GET["do"]) && $_GET["do"] == "eliminar"){ ?>
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg>
+                    <div class="ps-2">Borrado correctamente.</div>
             </div>
         <?php } ?>
         <div class="row">
